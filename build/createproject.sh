e3d3e95a521fd2f950bce189dc8a18f913ca1a93
Dave Green <dave@developerdave.co.uk>
2011-06-09 16:37:18 +0100

    Amended createproject.sh to include php files.

--- build/createproject.sh
+++ build/createproject.sh
@@ -42,7 +42,7 @@ else
     echo "Created Directory: $dst"
 
     cd -- "$src"
-    cp -vr -- css js img build test *.html *.xml *.txt *.png *.ico .htaccess "$dst"
+    cp -vr -- css js img build test *.html *.xml *.txt *.png *.php *.ico .htaccess "$dst"
 
     #sucess message
     echo "Created Project: $dst"
